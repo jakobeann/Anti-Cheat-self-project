633410f1e7319f36c06c799620fa54210c9de5e9
jakobeann <gykores@gmail.com>
2025-07-26 20:49:29 +0300

    Create AntiCheatClient.lua

---
+++ AntiCheatClient.lua
@@ -0,0 +1,56 @@
+local Players = game:GetService("Players")
+local RS = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local player = Players.LocalPlayer
+local char = player.Character or player.CharacterAdded:Wait()
+local humanoid = char:WaitForChild("Humanoid")
+local FlagEvent = RS:WaitForChild("RemoteEvents"):WaitForChild("AntiCheatFlag")
+
+-- GUI check
+task.spawn(function()
+	while true do
+		task.wait(2)
+		if not player:FindFirstChild("PlayerGui") then
+			FlagEvent:FireServer("UI Tampering")
+		end
+	end
+end)
+
+-- speed and jump check
+task.spawn(function()
+	while humanoid and humanoid.Parent do
+		task.wait(1)
+		if humanoid.WalkSpeed > 20 then
+			FlagEvent:FireServer("SpeedHack")
+		end
+		if humanoid.JumpPower > 50 then
+			FlagEvent:FireServer("SpeedHack")
+		end
+	end
+end)
+
+-- tool injection check
+task.spawn(function()
+	while true do
+		task.wait(3)
+		for _, tool in ipairs(player.Backpack:GetChildren()) do
+			if not tool:IsA("Tool") then continue end
+			if not tool:FindFirstChild("Handle") then
+				FlagEvent:FireServer("ToolInjection")
+			end
+		end
+	end
+end)
+
+-- fly detection (if the player is above the jump limit)
+local lastY = char:WaitForChild("HumanoidRootPart").Position.Y
+task.spawn(function()
+	while true do
+		task.wait(0.5)
+		local currentY = char.HumanoidRootPart.Position.Y
+		if (currentY - lastY) > 25 and humanoid:GetState() ~= Enum.HumanoidStateType.Freefall then
+			FlagEvent:FireServer("FlyHack")
+		end
+		lastY = currentY
+	end
+end)
